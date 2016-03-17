11b272d41d9b932ae701005bbdcfdf82ca697b2e
tourniquet <admyn3d@gmail.com>
2016-03-17 09:07:53 +0200

    table design

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,11 @@ var vm = new Vue({
   el: 'body',
 
   data: {
-    
+    markets: [
+      { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' },
+      { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' },
+      { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' },
+      { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' }
+    ]
   }
 })
