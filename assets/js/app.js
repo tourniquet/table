3ef0ba1fb17296389c6ca59af36997b2eb54c5f7
tourniquet <admyn3d@gmail.com>
2016-03-18 18:02:18 +0200

    v 16.3.18 add data from form to table

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,5 +8,36 @@ var vm = new Vue({
       { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' },
       { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' }
     ]
+  },
+
+  methods: {
+    addMarket () {
+      var self = this
+      $("form").on("submit", function (event) {
+        event.preventDefault()
+
+        var newId = +self.markets[self.markets.length - 1].id + 1
+        var result = $(this).serializeArray()
+        var data = {
+          id: newId,
+          title: result[0].value,
+          brand: result[1].value,
+          estSalesUsd: result[2].value + '$',
+          estSalesChf: '100$',
+          status: result[3].value,
+          actions: 'Remove'
+        }
+
+        self.markets.push(data)
+      })
+    },
+
+    editMarket (index) {
+
+    },
+
+    removeMarket (index) {
+      this.markets.splice(index, 1)
+    }
   }
 })
