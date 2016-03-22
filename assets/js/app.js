432e017c26c145bb340f1557de9e380d24a19ba3
tourniquet <admyn3d@gmail.com>
2016-03-22 17:46:28 +0200

    v 16.3.22 added edit method

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,17 +3,28 @@ var vm = new Vue({
 
   data: {
     markets: [
-      { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' },
-      { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' },
-      { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' },
-      { id: '1', title: 'Some', brand: 'Nokia', estSalesUsd: '123$', estSalesChf: '234$', status: 'Alive', action: 'Remove' }
-    ]
+      { id: '1', title: 'Some', brand: 'Nokia', launch: 'september', estSalesUsd: '123', estSalesEur: '234', status: 'Alive' },
+      { id: '2', title: 'Other', brand: 'Canon', launch: 'august', estSalesUsd: '750', estSalesEur: '480', status: 'Dead' }
+    ],
+
+    form: {
+      id: '',
+      title: '',
+      brand: '',
+      launch: '',
+      estSalesUsd: '',
+      estSalesEur: '',
+      status: ''
+    },
+
+    button: 'add market'
   },
 
   methods: {
     addMarket () {
       var self = this
-      $("form").on("submit", function (event) {
+
+      $('form').on('submit', function (event) {
         event.preventDefault()
 
         var newId = +self.markets[self.markets.length - 1].id + 1
@@ -22,22 +33,52 @@ var vm = new Vue({
           id: newId,
           title: result[0].value,
           brand: result[1].value,
-          estSalesUsd: result[2].value + '$',
-          estSalesChf: '100$',
-          status: result[3].value,
-          actions: 'Remove'
+          launch: result[2].value,
+          estSalesUsd: result[3].value,
+          estSalesEur: Math.round(result[3].value * 0.89),
+          status: result[4].value
         }
 
         self.markets.push(data)
+        vm.clearInputs()
       })
     },
 
     editMarket (index) {
+      this.button = 'edit market'
+      this.form = this.markets[index]
 
+      var inputs = document.getElementsByClassName('mdl-textfield--floating-label')
+      console.log(inputs)
+      var intputsArray = Array.from(inputs)
+      intputsArray.forEach(function (item) {
+        item.className += ' is-dirty'
+      })
+
+      this.markets.splice(index, 1, this.form)
     },
 
     removeMarket (index) {
       this.markets.splice(index, 1)
+    },
+
+    clearInputs () {
+      var inputs = document.getElementById('form').getElementsByClassName('mdl-textfield__input')
+      var intputsArray = Array.from(inputs)
+      intputsArray.forEach(function (item) {
+        item.value = ''
+      })
+
+      vm.removeFloatingLabel()
+      this.button = 'add market'
+    },
+
+    removeFloatingLabel () {
+      var inputs = document.getElementById('form').getElementsByClassName('is-dirty')
+      var intputsArray = Array.from(inputs)
+      intputsArray.forEach(function (item) {
+        item.classList.remove('is-dirty')
+      })
     }
   }
 })
